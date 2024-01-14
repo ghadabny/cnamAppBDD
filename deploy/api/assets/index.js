a3dd104ef7c60f2387da38f2055b4ed4cafdb32a
ghadabenyounes7 <ghadabenyounes7@gmail.com>
2024-01-14 21:37:29 +0100

    m

---
+++ deploy/api/assets/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const cors = require("cors");
+const sequelize = require('./database/sequelize'); 
+
+
+const app  = express ();
+
+var corsOptions = {
+  origin: "*",
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  headers: 'Content-Type, Authorization',
+  exposedHeaders:'Authorization'
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+app.use('/images', express.static('imgAng'));
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+require("./routes")(app);
+
+// set port, listen for requests
+const PORT =  443;
+
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}.`);
+// });
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connexion à la base de données réussie.');
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}.`);
+    });
+  })
+  .catch(err => {
+    console.error('Impossible de se connecter à la base de données:', err);
+  });
